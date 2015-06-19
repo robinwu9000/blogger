80a406a60fda838a939a9de518f158e5318bd8e3
robinwu9000 <robwu@ucdavis.edu>
2015-06-19 07:05:43 +0000

    Sorcery authentication added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618034902) do
+ActiveRecord::Schema.define(version: 20150618234805) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20150618034902) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
+
+  create_table "authors", force: true do |t|
+    t.string   "username",         null: false
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authors", ["email"], name: "index_authors_on_email", unique: true
 
   create_table "comments", force: true do |t|
     t.string   "author_name"
